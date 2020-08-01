865f1a672d36478026ed29d0a717e5249c826d83
e.yakovlev <eafifa.lover@gmail.com>
2020-08-02 01:05:08 +0300

    build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
+    publicPath: '/rick-and-morty-vue/dist/',
     configureWebpack: {
         plugins: [
             // new BundleAnalyzerPlugin()
